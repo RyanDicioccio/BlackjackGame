426b549251a74e73cf34e4598283bd0f87d62796
RyanDicioccio <ryandicioccio05@gmail.com>
2024-02-08 22:33:54 -0500

    initialize repo

---
+++ src/Program.java
@@ -0,0 +1,17 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+	
+		int cardCount = 52;
+		int playerCards = 0;
+		int dealerCards = 0;
+		
+		String[] arr1={"A","2","3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"};
+		
+		String newString = arr1[0] + arr1[1];
+		System.out.println(newString);
+		
+	}
+
+}
